326e3d85fd4c8f1688bb007902b6161d166c4dc4
jlaikine <78849823+jlaikine@users.noreply.github.com>
2022-12-22 09:46:26 +0100

    Add files via upload

---
+++ src/application/VueController.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VueController{
+
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
